294798e6f0036b5988d95d73de9628969f97dcb1
Daniel Fernandes <daniel.f2005@aluno.ifsc.edu.br>
2024-08-05 07:23:56 -0300

    Aula enum

---
+++ 2024-07-31/app/src/main/java/engtelecom/poo/sistemasolar/SistemaSolar.java
@@ -0,0 +1,8 @@
+package engtelecom.poo.sistemasolar;
+
+public enum SistemaSolar {
+    MERCURIO(0), VENUS(1), TERRA(2), MARTE(3), JUPITER(4), SATURNO(5), URANO(6), NETUNO(7);
+
+    SistemaSolar(int i) {
+    }
+}
